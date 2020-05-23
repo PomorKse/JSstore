e571aaad01a66ae1a6f8b0a3a172d97a9d280504
kseniya <kseniia.pomortseva@gmail.com>
2020-05-23 16:10:11 +0300

    empty classes for cart.cartItem

--- script.js
+++ script.js
@@ -1,26 +1,70 @@
-const goods = [
-  { title: 'Shirt', price: 150 },
-  { title: 'Socks', price: 50 },
-  { title: 'Jacket', price: 350 },
-  { title: 'Shoes', price: 250 },
-];
+class ProductsList {
+  constructor(container = '.products') {
+    this.container = container;
+    this.goods = goods;
+    this.allProducts = [];
+    this._fetchProduct();
+    this.render();
+    this.total();
+  }
+  _fetchProduct(){
+    this.goods = [
+      { title: 'Shirt', price: 150 },
+      { title: 'Socks', price: 50 },
+      { title: 'Jacket', price: 350 },
+      { title: 'Shoes', price: 250 },
+    ];
+  }
 
-// let [{title, price}] = goods; деструктуризация массива?
-//Не понимаю, где добавить значение по умолчанию.
-//и как это должно работать
+  render(){
+    const block = document.querySelector(this.container);
+    for (let product of this.goods) {
+      const productObj = new ProductItem(product);
+      this.allProducts.push(productObj);
+      block.innerHTML += productObj.render();
 
-const renderGoodsItem = (item) => {
-  return `<div class="goods-item">
-  <h3>${item.title}</h3>
-  <p>${item.price}</p>
-  <button class="buy-btn">BUY</button>
-  </div>`;
-};
+    }
+  }
 
-const renderGoodsList = (list) => {
-  let goodsList = list.map(item => renderGoodsItem(item));
-  let newGoodsList = goodsList.join('');
-  document.querySelector('.goods-list').innerHTML = newGoodsList;
+  total(){
+    let totalPrice = 0;
+    this.goods.forEach(item => totalPrice+=item.price);
+    return totalPrice;
+  }
 }
 
-renderGoodsList(goods);
+class ProductItem {
+  constructor(product, img = "https://placehold.it/200x150"){
+    this.title = product.title;
+    this.id = product.id;
+    this.img = product.img;
+    this.price = product.price;
+  }
+
+  render(){
+    return `<div class="goods-item">
+    <img alt="some img" src="${this.img}">
+    <h3>${item.title}</h3>
+    <p>${item.price}</p>
+    <button class="buy-btn">BUY</button>
+    </div>`;
+  }
+}
+
+// class Cart {
+//   constructor(container = '.?'') {
+//     this.container = container;
+//     this.goods = goods;
+//     this.render();
+//     this.addProduct();
+//     this.deleteProduct();
+//   }
+// }
+//
+// class CartItem {
+//   constructor() {
+//
+//   }
+// }
+
+let list = new ProductsList();
